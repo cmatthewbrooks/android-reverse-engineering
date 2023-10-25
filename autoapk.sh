faf5280e536b295d19d73956e3460dc980de2a73
Matt B <3974104+cmatthewbrooks@users.noreply.github.com>
2023-10-26 07:10:42 +0800

    Update autoapk script

--- autoapk.sh
+++ autoapk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#set -x
+# set -x
 
 # This value needs to be whatever was used when the image was built
 IMAGE="android-re"
@@ -10,28 +10,33 @@ TARGET_APK=$1
 FNAME=$(basename "$TARGET_APK" | cut -d. -f1)
 WORKING_DIR="/private/tmp/$TIMESTAMP-$FNAME-autoapk"
 ORIGINAL_COPY="$WORKING_DIR/original.apk"
-UNZIPPED="unzipped"
-APKTOOL="apktool"
+UNZIPPED="$WORKING_DIR/unzipped"
+APKTOOL="$WORKING_DIR/apktool"
+
+
+if ! docker info > /dev/null 2>&1; then
+  echo "This script uses docker, and it isn't running - please start docker and try again!"
+  exit 1
+fi
+
 
 mkdir -p $WORKING_DIR
-cd $WORKING_DIR
-
 cp $TARGET_APK $ORIGINAL_COPY
 
 echo "[+] Unzipping APK..."
 mkdir -p $UNZIPPED
-unzip -qq $ORIGINAL_COPY -d $UNZIPPED
+unzip -o -qq $ORIGINAL_COPY -d $UNZIPPED
 echo "[+] Unzipping APK...DONE"
 
 echo "[+] Extracting with apktool..."
-docker run --rm -it -v $PWD:/data $IMAGE /tools/apktool/apktool d "original.apk" -o $APKTOOL
+docker run --rm -it -v $WORKING_DIR:/data $IMAGE /tools/apktool/apktool d "original.apk" -o "apktool"
 echo "[+] Extracting with apktool...DONE"
 
 echo "[+] Extracting with dex2jar..."
-docker run --rm -it -v $PWD:/data $IMAGE /tools/dex-tools/d2j-dex2jar.sh $UNZIPPED/classes.dex -o "dex2jar.jar"
+docker run --rm -it -v $WORKING_DIR:/data $IMAGE /tools/dex-tools/d2j-dex2jar.sh "unzipped/classes.dex" -o "dex2jar.jar"
 echo "[+] Extracting with dex2jar...DONE"
 
 echo "[+] Decompiling with jd-cli..."
 mkdir -p "jd"
-docker run --rm -it -v $PWD:/data $IMAGE /tools/jd-cmd/jd-cli -od "jd" "dex2jar.jar"
+docker run --rm -it -v $WORKING_DIR:/data $IMAGE /tools/jd-cmd/jd-cli -od "classes_decompiled" "dex2jar.jar"
 echo "[+] Decompiling with jd-cli...DONE"
